042510dc79652b10c67cd30368d380b55cc068d3
Christine Choo <90698240+itschristi@users.noreply.github.com>
2022-02-10 11:47:26 +0800

    Support Board V1

---
+++ supportboard/config.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * ==========================================================
+ * INITIAL CONFIGURATION FILE
+ * ==========================================================
+ *
+ * Insert here the information for the database connection and for other core settings.
+ *
+ */
+
+/* Plugin folder url */
+define('SB_URL', 'https://astrokids.com.my/wp-content/plugins/supportboard/supportboard');
+
+/* The name of the database */
+define('SB_DB_NAME', 'wp_lcbms');
+
+/* MySQL database username */
+define('SB_DB_USER', 'wp_mc5zh');
+
+/* MySQL database password */
+define('SB_DB_PASSWORD', 'z3EL$dCk25');
+
+/* MySQL hostname */
+define('SB_DB_HOST', 'localhost:3306');
+
+/* MySQL port (optional) */
+define('SB_DB_PORT', '');
+
+/* WordPress prefix */
+define('SB_WP_PREFIX', '8BvIV1Bl0_');
+
+/* Upload path */
+define('SB_UPLOAD_PATH', '/var/www/vhosts/astrokids.com.my/httpdocs/wp-content/uploads/sb');
+
+/* Upload url */
+define('SB_UPLOAD_URL', 'https://astrokids.com.my/wp-content/uploads/sb');
+
+/* [extra] */
+
+?>
